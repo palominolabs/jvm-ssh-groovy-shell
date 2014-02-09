fc2d473d85871c45bf76ffb7e4a17adbdb7309c5
Marshall Pierce <github@contact.mpierce.org>
2014-02-09 10:32:04 -0800

    Javadoc

--- authorized-keys-ssh-authenticator/src/main/java/com/palominolabs/ssh/auth/publickey/AuthorizedKey.java
+++ authorized-keys-ssh-authenticator/src/main/java/com/palominolabs/ssh/auth/publickey/AuthorizedKey.java
@@ -2,6 +2,9 @@ package com.palominolabs.ssh.auth.publickey;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Encapsulates the result of parsing a line of an SSH authorized_keys file.
+ */
 public final class AuthorizedKey {
     private final String type;
     private final byte[] data;
@@ -13,16 +16,25 @@ public final class AuthorizedKey {
         this.comment = comment;
     }
 
+    /**
+     * @return key type (e.g. "ssh-rsa")
+     */
     @Nonnull
     public String getType() {
         return type;
     }
 
+    /**
+     * @return encoded key data in RFC 4253 format
+     */
     @Nonnull
     public byte[] getData() {
         return data;
     }
 
+    /**
+     * @return key comment
+     */
     @Nonnull
     public String getComment() {
         return comment;
