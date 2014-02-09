0954c29b2c9e3d34ea38b5ee855b74a89c5147dd
Marshall Pierce <github@contact.mpierce.org>
2014-02-08 17:12:08 -0800

    Use authorized_keys authenticator if user provides arg

--- demo-app/src/main/java/com/palominolabs/groovy/ssh/demo/DemoMain.java
+++ demo-app/src/main/java/com/palominolabs/groovy/ssh/demo/DemoMain.java
@@ -1,23 +1,40 @@
 package com.palominolabs.groovy.ssh.demo;
 
+import com.google.common.base.Supplier;
+import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.ServletModule;
-import com.palominolabs.ssh.groovy.GroovyShellCommandFactory;
 import com.palominolabs.http.server.HttpServerConnectorConfig;
 import com.palominolabs.http.server.HttpServerWrapperConfig;
 import com.palominolabs.http.server.HttpServerWrapperFactory;
 import com.palominolabs.http.server.HttpServerWrapperModule;
+import com.palominolabs.ssh.auth.publickey.AuthorizedKeysPublicKeyDataSource;
+import com.palominolabs.ssh.auth.publickey.AuthorizedKeysPublickeyAuthenticator;
+import com.palominolabs.ssh.auth.publickey.PublicKeyDataSource;
+import com.palominolabs.ssh.auth.publickey.PublicKeyMatcherFactory;
+import com.palominolabs.ssh.auth.publickey.dsa.DsaPublicKeyMatcherFactory;
+import com.palominolabs.ssh.auth.publickey.rsa.RsaPublicKeyMatcherFactory;
+import com.palominolabs.ssh.groovy.GroovyShellCommandFactory;
 import groovy.lang.Binding;
 import org.apache.sshd.SshServer;
 import org.apache.sshd.common.NamedFactory;
+import org.apache.sshd.server.PublickeyAuthenticator;
 import org.apache.sshd.server.UserAuth;
 import org.apache.sshd.server.auth.UserAuthNone;
+import org.apache.sshd.server.auth.UserAuthPublicKey;
 import org.apache.sshd.server.keyprovider.PEMGeneratorHostKeyProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -27,7 +44,24 @@ public final class DemoMain {
 
         startHttpServer(injector);
 
-        startSshServer(injector);
+        PublickeyAuthenticator authenticator = null;
+        if (args.length == 1) {
+            // user provided an authorized_keys path
+            final File authorizedKeys = new File(args[0]);
+
+            // we want to read RSA and DSA keys
+            List<PublicKeyMatcherFactory> factories =
+                Lists.newArrayList(new RsaPublicKeyMatcherFactory(), new DsaPublicKeyMatcherFactory());
+
+            // read keys from an ssh authorized_keys file
+            PublicKeyDataSource dataSource = new AuthorizedKeysPublicKeyDataSource(
+                new InputStreamSupplier(authorizedKeys));
+
+            // configure the authenticator with the above
+            authenticator = new AuthorizedKeysPublickeyAuthenticator(factories, dataSource);
+        }
+
+        startSshServer(injector, authenticator);
     }
 
     private static void startHttpServer(Injector injector) throws Exception {
@@ -41,6 +75,7 @@ public final class DemoMain {
         return Guice.createInjector(new AbstractModule() {
             @Override
             protected void configure() {
+                binder().requireExplicitBindings();
                 install(new HttpServerWrapperModule());
                 install(new ServletModule() {
                     @Override
@@ -55,12 +90,13 @@ public final class DemoMain {
         });
     }
 
-    private static void startSshServer(Injector injector) throws IOException {
+    private static void startSshServer(Injector injector, @Nullable PublickeyAuthenticator authenticator) throws
+        IOException {
         SshServer sshd = SshServer.setUpDefaultServer();
 
         sshd.setPort(10222);
 
-        sshd.setKeyPairProvider(new PEMGeneratorHostKeyProvider("hostkey.pem", "DSA", 1024));
+        sshd.setKeyPairProvider(new PEMGeneratorHostKeyProvider("demo-hostkey.pem", "DSA", 1024));
 
         Binding binding = new Binding();
         binding.setProperty("injector", injector);
@@ -68,11 +104,35 @@ public final class DemoMain {
 
         Executor executor = Executors.newCachedThreadPool();
         sshd.setShellFactory(new GroovyShellCommandFactory(binding, executor));
-        ArrayList<NamedFactory<UserAuth>> userAuthFactories = new ArrayList<NamedFactory<UserAuth>>();
-        userAuthFactories.add(new UserAuthNone.Factory());
-        sshd.setUserAuthFactories(userAuthFactories);
+
+        if (authenticator == null) {
+            // allow all users
+            sshd.setUserAuthFactories(Lists.<NamedFactory<UserAuth>>newArrayList(new UserAuthNone.Factory()));
+        } else {
+            sshd.setUserAuthFactories(Lists.<NamedFactory<UserAuth>>newArrayList(new UserAuthPublicKey.Factory()));
+            sshd.setPublickeyAuthenticator(authenticator);
+        }
 
         // starts a daemon thread; you'll need something else running to keep the jvm up
         sshd.start();
+    }
+
+    private static class InputStreamSupplier implements Supplier<InputStream> {
+        private static final Logger logger = LoggerFactory.getLogger(InputStreamSupplier.class);
+        private final File authorizedKeys;
+
+        private InputStreamSupplier(File authorizedKeys) {
+            this.authorizedKeys = authorizedKeys;
+        }
+
+        @Override
+        public InputStream get() {
+            try {
+                return new FileInputStream(authorizedKeys);
+            } catch (FileNotFoundException e) {
+                logger.warn("Couldn't load key file", e);
+                return null;
+            }
+        }
     }
 }
